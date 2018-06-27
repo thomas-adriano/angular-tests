d7d3f5266a0efac3ba6c61a6bbd8d5af920af2a7
Thomas O. Adriano <thomas.o.adriano@gmail.com>
2018-06-27 19:18:10 -0300

    animation bezier cube finished, code cleaned

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,91 +1,12 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {
-  trigger,
-  style,
-  animate,
-  transition,
-  query,
-  state,
-  group
-} from '@angular/animations';
+import { routeFlyInOut } from './shared/animations/route-fly-in-out';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations: [
-    trigger('flyInOut', [
-      transition('* => *', [
-        // The query function has three params.
-        // First is the event, so this will apply on entering or when the element is added to the DOM.
-        // Second is a list of styles or animations to apply.
-        // Third we add a config object with optional set to true, this is to signal
-        // angular that the animation may not apply as it may or may not be in the DOM.
-        query(
-          ':enter',
-          [
-            style({
-              transform: 'translateX(-100%)',
-              opacity: 0
-            })
-          ],
-          {
-            optional: true
-          }
-        ),
-        query(
-          ':leave',
-          [
-            style({
-              transform: 'translateX(0)',
-              opacity: 1
-            }),
-            group([
-              animate(
-                '0.133s ease-out',
-                style({ transform: 'translateX(100%)' })
-              ),
-              animate('0.133s ease-out', style({ opacity: 0 }))
-            ])
-          ],
-          { optional: true }
-        ),
-        query(
-          ':enter',
-          [
-            style({
-              transform: 'translateX(-100%)',
-              opacity: 0
-            }),
-            group([
-              animate(
-                '0.233s ease-in',
-                style({
-                  transform: 'translateX(0)'
-                })
-              ),
-              animate(
-                '0.233s ease-in',
-                style({
-                  opacity: 1
-                })
-              )
-            ])
-          ],
-          { optional: true }
-        )
-      ])
-      // state('in', style({ transform: 'translateX(0)' })),
-      // transition(':enter', [
-      //   style({ transform: 'translateX(-100%)' }),
-      //   animate(225)
-      // ]),
-      // transition(':leave', [
-      //   animate(225, style({ transform: 'translateX(100%)' }))
-      // ])
-    ])
-  ]
+  animations: [routeFlyInOut()]
 })
 export class AppComponent implements AfterViewInit {
   title = 'app';
---
+++ src/app/shared/animations/route-fly-in-out.ts
@@ -0,0 +1,60 @@
+import {
+  trigger,
+  transition,
+  query,
+  style,
+  animate
+} from '@angular/animations';
+
+/**
+ * Animação para a troca de rotas, exemplo de uso:
+ * <section [@flyInOut]="ro.isActivated ? ro.activatedRoute : ''">
+ *  <router-outlet #ro="outlet">
+ *  </router-outlet>
+ * </section>
+ *
+ */
+export function routeFlyInOut() {
+  return trigger('flyInOut', [
+    transition('* => *', [
+      query(
+        ':enter',
+        [
+          style({
+            transform: 'translateX(-100%)',
+            opacity: 0
+          })
+        ],
+        {
+          optional: true
+        }
+      ),
+      query(
+        ':leave',
+        [
+          animate(
+            '0.200s ease-out',
+            style({
+              transform: 'translateX(100%)',
+              opacity: 0
+            })
+          )
+        ],
+        { optional: true }
+      ),
+      query(
+        ':enter',
+        [
+          animate(
+            '0.400s cubic-bezier(.57,.96,.66,.9)',
+            style({
+              transform: 'translateX(0)',
+              opacity: 1
+            })
+          )
+        ],
+        { optional: true }
+      )
+    ])
+  ]);
+}
